a9d6a4938444d2e6b7cb158664409825bca8e6ba
Tom Barrett <tbtommyb@gmail.com>
2016-12-26 18:25:25 +0000

    Removed commented out require statements

--- src/backend/server/server.js
+++ src/backend/server/server.js
@@ -5,13 +5,6 @@ import injectThen from "inject-then";
 import routes from "./routes";
 
 const server = new Hapi.Server();
-/*
-const Hapi = require("hapi");
-const Inert = require("inert");
-const Vision = require("vision");
-const injectThen = require("inject-then");
-const routes = require("./routes.js");
-const server = new Hapi.Server();*/
 
 require("babel-register")({
     presets: ["es2015", "react"]
