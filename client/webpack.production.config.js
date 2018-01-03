6466d623fc17842d9a306eb8353b37f4f389e602
Tom Barrett <tbtommyb@gmail.com>
2018-01-03 16:08:18 +0000

    Removed redundant uglifier plugin

--- client/webpack.production.config.js
+++ client/webpack.production.config.js
@@ -11,11 +11,6 @@ var productionPlugins = [
             "NODE_ENV": JSON.stringify("production")
         }
     }),
-    new webpack.optimize.UglifyJsPlugin({
-        compress: {
-            warnings: true
-        }
-    })
 ];
 
 var productionConfig = Object.assign({}, browserConfig, config);
